7bfcfa90f020249a2cb188309f2c6f2a12e75bd3
lulucodingwizard <how.to.develop.fast@gmail.com>
2022-09-27 10:33:28 +1000

    lesson 14 add toggle button using css and jquery

--- app.js
+++ app.js
@@ -3,4 +3,15 @@ $(document).ready(function () {
     $("#btn-menu").toggleClass("isOpen");
     $("#right-menu-section").toggle();
   });
+
+  // detect checkboxes toggle button status
+  $("#toggle_button").change(function () {
+    if ($(this).is(":checked")) {
+      $("#monthly_label").removeClass("selected");
+      $("#yearly_label").addClass("selected");
+    } else {
+      $("#monthly_label").addClass("selected");
+      $("#yearly_label").removeClass("selected");
+    }
+  });
 });
